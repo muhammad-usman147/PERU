1421e3f0bd3ddad4cc71d52e074c70f0c04e7c94
Muhammad Usman Shakeel <ushakeel808@gmail.com>
2023-09-13 03:12:08 +0500

    main-user-stories-added

--- gui.py
+++ gui.py
@@ -4,10 +4,10 @@ import subprocess
 import tkinter.font as tkfont
 import requests 
 from tkinter import DISABLED
+import cv2
 
 
-
-customtkinter.set_appearance_mode('white')
+#customtkinter.set_appearance_mode('dark')
 root = customtkinter.CTk()
 root.geometry("1080x550")
 
@@ -54,9 +54,9 @@ def on_hover(event):
 def on_leave(event):
     vis_resig_emp.configure(text_color='Green',fg_color='Orange',bg_color='Orange')
 
-frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=20, padx=60, fill='both', expand=True)
-frame.configure(width=500)
+
+frame = customtkinter.CTkFrame(master=root,width=2000)
+frame.pack(pady=20, padx=20 ,fill='both', expand=True)
 button_font = customtkinter.CTkFont(size=20)
 button_font2 = customtkinter.CTkFont(size=18,weight='bold')
 
@@ -91,14 +91,148 @@ train_button.grid(row=4, column=0, columnspan=2, pady=12, padx=10, sticky="ew")
 
 
 #HU-05 : System Predictions
-predict_button = customtkinter.CTkButton(master=inner_frame, text='U-05: Predict', command = '#',
-                                        bg_color='Blue',fg_color='Blue', font=button_font)
-predict_button.grid(row=5,column=2,columnspan=2,pady=12,padx=10,sticky='ew')
+predict_button = customtkinter.CTkButton(master=inner_frame, text='HU-05: Predict', command = '#',
+                                        bg_color='#3d3db8',fg_color='#3d3db8', font=button_font)
+predict_button.grid(row=5,column=0,columnspan=2,pady=12,padx=10,sticky='ew')
 
 #HU-06: Visualize possible regisnation of employees from there table
-vis_resig_emp = customtkinter.CTkButton(master=inner_frame,text='HU-06: Show Resignations',command='#',
-                                        fg_color='Orange',font=button_font2,text_color='Green',hover_color='LightBlue')
-vis_resig_emp.grid(row=5,column=2,columnspan=1,pady=12,padx=10,sticky='ew',)
+vis_resig_emp = customtkinter.CTkButton(master=inner_frame,text='HU-06: Show Possible \n Resignations',command='#',
+                                        fg_color='Orange',font=button_font2,text_color='Green')
+vis_resig_emp.grid(row=1,column=2,columnspan=1,pady=12,padx=5,sticky='ew',)
 vis_resig_emp.bind("<Enter>",on_hover)
 vis_resig_emp.bind("<Leave>",on_leave)
+
+
+#HU-07: Visualize possible regisnation of employees from there table
+
+def on_hover1(event):
+    vis_resig_emp2.configure(text_color='LightBlue',fg_color='Green',bg_color='Green')
+def on_leave1(event):
+    vis_resig_emp2.configure(text_color='Green',fg_color='Orange',bg_color='Orange')
+
+vis_resig_emp2 = customtkinter.CTkButton(master=inner_frame,text='HU-07: Show Voluntary \n Resignations',command='#',
+                                        fg_color='Orange',font=button_font2,text_color='Green')
+vis_resig_emp2.grid(row=2,column=2,columnspan=1,pady=5,padx=5,sticky='ew',)
+
+
+vis_resig_emp2.bind("<Enter>",on_hover1)
+vis_resig_emp2.bind("<Leave>",on_leave1)
+#HU-08: Display Predictibility Meter
+
+def on_hover3(event):
+    vis_resig_emp3.configure(text_color='LightBlue',fg_color='Green',bg_color='Green')
+def on_leave3(event):
+    vis_resig_emp3.configure(text_color='Green',fg_color='Orange',bg_color='Orange')
+
+vis_resig_emp3 = customtkinter.CTkButton(master=inner_frame,text='HU-08: Display Predictibility \n Meter',command='#',
+                                        fg_color='Orange',font=button_font2,text_color='Green')
+vis_resig_emp3.grid(row=1,column=3,columnspan=1,pady=5,padx=5,sticky='ew',)
+
+
+vis_resig_emp3.bind("<Enter>",on_hover3)
+vis_resig_emp3.bind("<Leave>",on_leave3)
+
+#HU-09: Download Table
+
+def on_hover4(event):
+    vis_resig_emp4.configure(text_color='LightBlue',fg_color='Green',bg_color='Green')
+def on_leave4(event):
+    vis_resig_emp4.configure(text_color='Green',fg_color='Orange',bg_color='Orange')
+
+vis_resig_emp4 = customtkinter.CTkButton(master=inner_frame,text='HU-09: Download \n Predictions(PDF)',command='#',
+                                        fg_color='Orange',font=button_font2,text_color='Green')
+vis_resig_emp4.grid(row=2,column=3,columnspan=1,pady=5,padx=5,sticky='ew',)
+
+
+vis_resig_emp4.bind("<Enter>",on_hover4)
+vis_resig_emp4.bind("<Leave>",on_leave4)
+
+#HU-10 - HU - 13 Display Charts
+def on_hover5(event):
+    vis_resig_emp5.configure(text_color='LightBlue',fg_color='Green',bg_color='Green')
+def on_leave5(event):
+    vis_resig_emp5.configure(text_color='Green',fg_color='Orange',bg_color='Orange')
+
+vis_resig_emp5 = customtkinter.CTkButton(master=inner_frame,text='HU-1013: Show Charts',command='#',
+                                        fg_color='Orange',font=button_font2,text_color='Green')
+vis_resig_emp5.grid(row=3,column=2,columnspan=1,pady=5,padx=5,sticky='ew',)
+vis_resig_emp5.bind("<Enter>",on_hover5)
+vis_resig_emp5.bind("<Leave>",on_leave5)
+#HU-14 Display Variables
+def on_hover6(event):
+    vis_resig_emp6.configure(text_color='LightBlue',fg_color='Green',bg_color='Green')
+def on_leave6(event):
+    vis_resig_emp6.configure(text_color='Green',fg_color='Orange',bg_color='Orange')
+
+vis_resig_emp6 = customtkinter.CTkButton(master=inner_frame,text='HU-14: Display Variables',command='#',
+                                        fg_color='Orange',font=button_font2,text_color='Green')
+vis_resig_emp6.grid(row=4,column=2,columnspan=1,pady=5,padx=5,sticky='ew',)
+vis_resig_emp6.bind("<Enter>",on_hover6)
+vis_resig_emp6.bind("<Leave>",on_leave6)
+
+#HU-15 Download All Charts
+def on_hover7(event):
+    vis_resig_emp7.configure(text_color='LightBlue',fg_color='Green',bg_color='Green')
+def on_leave7(event):
+    vis_resig_emp7.configure(text_color='Green',fg_color='Orange',bg_color='Orange')
+
+vis_resig_emp7 = customtkinter.CTkButton(master=inner_frame,text='HU-15: Download Graphs',command='#',
+                                        fg_color='Orange',font=button_font2,text_color='Green')
+vis_resig_emp7.grid(row=3,column=3,columnspan=1,pady=5,padx=5,sticky='ew',)
+vis_resig_emp7.bind("<Enter>",on_hover7)
+vis_resig_emp7.bind("<Leave>",on_leave7)
+
+
+
+#HU-16 Download Table
+def on_hover8(event):
+    vis_resig_emp8.configure(text_color='LightBlue',fg_color='Green',bg_color='Green')
+def on_leave8(event):
+    vis_resig_emp8.configure(text_color='Green',fg_color='Orange',bg_color='Orange')
+
+vis_resig_emp8 = customtkinter.CTkButton(master=inner_frame,text='HU-15: Download Prediction \n Table (excel)',command='#',
+                                        fg_color='Orange',font=button_font2,text_color='Green')
+vis_resig_emp8.grid(row=5,column=2,columnspan=1,pady=5,padx=5,sticky='ew',)
+vis_resig_emp8.bind("<Enter>",on_hover8)
+vis_resig_emp8.bind("<Leave>",on_leave8)
+
+
+
+
+#HU-17 Download Table
+
+vis_resig_emp9 = customtkinter.CTkButton(master=inner_frame,text='HU-17: Exit',command='#',
+                                        fg_color='#ed1f37',bg_color='#ed1f37',font=button_font2,text_color='White')
+vis_resig_emp9.grid(row=6,column=3,columnspan=1,padx=5,sticky='ew',)
+
+#HU-18 Change Theme
+def change_theme(event):
+    selected_theme = theme.get()
+
+    if selected_theme == "Light Theme":
+        #root.tk_setPalette(background='#FFFFFF', foreground='#000000')
+        customtkinter.set_appearance_mode('light')
+
+    elif selected_theme == "Dark Theme":
+        #root.tk_setPalette(background='#000000', foreground='#FFFFFF')
+        customtkinter.set_appearance_mode('dark')
+
+theme = customtkinter.CTkComboBox(master=inner_frame, values = ['Light Theme','Dark Theme'],command=change_theme,
+                                    fg_color='Green',bg_color='Green',text_color='White')
+theme.grid(row=2,column=1,columnspan=1,padx=5,sticky='ew',)
+
+theme.set("Light")
+
+#HU-20 Help Button
+def displayGuide():
+    img = cv2.imread("guide.png")
+    cv2.imshow("System Guide",img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+help = customtkinter.CTkButton(master=inner_frame,text='HU-20: System Guide',command=displayGuide,
+                                        fg_color='#ed1f37',bg_color='#ed1f37',font=button_font2,text_color='White')
+help.grid(row=3,column=1,columnspan=1,padx=5,sticky='ew',)
+
 root.mainloop()
